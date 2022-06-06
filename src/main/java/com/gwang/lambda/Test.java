083347d08496b29ca0570176de230488f740bbd1
wanggang11 <wanggang11@meituan.com>
2022-06-06 17:14:00 +0800

    java 1.8新特性

--- src/main/java/com/gwang/lambda/Test.java
+++ src/main/java/com/gwang/lambda/Test.java
@@ -11,22 +11,37 @@ import java.util.stream.Stream;
 public class Test {
 
     public static void main(String[] args) {
+        //lambda写法
         Action action = (context) -> System.out.println(context);
-
         action.excute("12345");
 
-        BinaryOperator<Integer> operator = new BinaryOperator<Integer>() {
+        //传统匿名内部类实现写法
+        Action action1 = new Action() {
             @Override
-            public Integer apply(Integer integer, Integer integer2) {
-                return null;
+            public void excute(String context) {
+                System.out.println(context);
             }
         };
+        action1.excute("12345");
 
+        //lambda写法
+        //实现方法不带大括号
         BinaryOperator<Integer> operator1 = (x, y) -> x + y;
+        //当实现方法不只一行，需要带大括号
         BinaryOperator<Integer> operator2 = (x, y) -> {
             Integer a = x * 2 + y ;
             return a;
         };
+
+        //传统匿名内部类实现写法
+        BinaryOperator<Integer> operator = new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return integer + integer2;
+            }
+        };
+
+
 
         Function<String, String> function = (String string) -> string.toUpperCase();
         System.out.println(function.apply("a"));
